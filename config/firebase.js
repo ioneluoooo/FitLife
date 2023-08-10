fed84ab36ba012d7f75d2f1a8969cb09d6e88aed
ioneluoooo <101887923+ioneluoooo@users.noreply.github.com>
2023-08-10 20:54:37 +0300

    Add files via upload

---
+++ config/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import Constants from 'expo-constants';
+import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
+// Firebase config
+const firebaseConfig = {
+  apiKey: Constants.manifest.extra.apiKey,
+  authDomain: Constants.manifest.extra.authDomain,
+  projectId: Constants.manifest.extra.projectId,
+  storageBucket: Constants.manifest.extra.storageBucket,
+  messagingSenderId: Constants.manifest.extra.messagingSenderId,
+  appId: Constants.manifest.extra.appId,
+  databaseURL: Constants.manifest.extra.databaseURL,
+};
+// initialize firebase
+initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const firestore = getFirestore();
+export const storage = getStorage();
+export const database= getDatabase();
